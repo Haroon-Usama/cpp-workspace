a041a2294b9c591387d852bd5025388a25f3fe3a
Haroon-Usama <haroonusama07@gmail.com>
2026-09-10 16:05:52 +0500

    completed 4 functions Calculaotr

--- personal/calculator.cpp
+++ personal/calculator.cpp
@@ -4,6 +4,45 @@ using namespace std;
 
 int main() 
 {
-    cout << "Making a calculator in C++" << endl;
-    return 0;
+    double x , y;
+    
+    char op ;  
+    
+    cout << "Enter Two Numbers : ";
+    
+    cin >> x >> y ;
+    
+    cout << "Enter an operator (+, -, *, / ):";
+
+    cin >> op;
+    
+    if (op == '+')
+        {
+            cout << "The sum of " << x << " and " << y <<" is " << x+y << "\n";
+        }
+    else if (op == '-')
+        {
+        cout << "The Difference of " <<x << " and " << y <<" is " << x-y << endl;
+        }
+    else if (op == '*')    
+    {
+        cout << " The product of " << x <<" and " << y <<" is " << x*y << endl ;
+    }
+    else if (op == '/')
+    {
+        if(y != 0)
+            {
+                cout << "The quotient of " << x <<" and " << y << " is " << x/y << endl ;
+            }
+        else
+            {
+                cout << " Division By zero is not allowed " << endl;
+            }
+    }
+    else 
+
+    
+        cout <<" Invalid Operator " << endl;
+
+
 }
